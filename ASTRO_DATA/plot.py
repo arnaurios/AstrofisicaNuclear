6d08456d62633010bd5320b5ee2492ee582cf3e0
arnaurios <a.rios@surrey.ac.uk>
2021-03-18 18:22:35 +0000

    Updated WD astro data.

--- ASTRO_DATA/plot.py
+++ ASTRO_DATA/plot.py
@@ -8,18 +8,24 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 
-df = pd.read_csv (r'Mass_Radius_WD.csv')
+df = pd.read_csv (r'Mass_Radius_WD.csv',skiprows=4)
 print (df)
 
 #df.plot(x ='RadH', y='MassH', kind = 'scatter')
 #df.errorbar(x ='RadH', y='MassH', yerr='e_MassH')
-df[['RadH','MassH','e_RadH','e_MassH']].to_numpy()
-plt.show()
-#fig, ax = plt.subplots()
-#plt.plot( WD_radius,WD_mass,'o')
-#plt.xlabel('Radial coordinate [km]')
-#plt.xlim(0,30000)
-#plt.ylabel('Mass profile [M_sun]')
-#plt.ylim(0,1.5)
+df[['RadG','MassG','e_RadG','e_MassG']].to_numpy()
+
+units_radius=696340/100
+radius=df.RadG*units_radius
+error_radius=df.e_RadG*units_radius
+mass=df.MassH
+error_mass=df.e_MassG
+fig, ax = plt.subplots()
+plt.errorbar(radius,mass,xerr=error_radius,yerr=error_mass,fmt='ok')
+plt.xlabel('Radius [km]')
+plt.xlim(0,20000)
+plt.ylabel('Mass [M_sun]')
+plt.ylim(0,1.5)
 ##fig.savefig("WD.pdf")
 #plt.show()
+plt.show()
